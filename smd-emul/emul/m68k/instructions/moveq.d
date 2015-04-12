1f80b5562fd1533c808591205af36948b63137a5
Ivan Butygin <ivan.butygin@gmail.com>
2015-04-12 22:54:22 +0300

    fix

--- smd-emul/emul/m68k/instructions/moveq.d
+++ smd-emul/emul/m68k/instructions/moveq.d
@@ -6,20 +6,21 @@ package pure nothrow:
 void addMoveqInstructions(ref Instruction[ushort] ret)
 {
     //moveq
-    foreach(r; TupleRange!(0,8))
+    foreach(r; 0..8)
     {
         foreach(val; 0..0xff)
         {
             const instr = 0x7000 | (r << 9) | val;
-            ret.addInstruction(Instruction("moveq",cast(ushort)instr,0x2,&moveqImpl!r));
+            ret.addInstruction(Instruction("moveq",cast(ushort)instr,0x2,&moveqImpl!void));
         }
     }
 }
 
 private:
-void moveqImpl(int Reg)(CpuPtr cpu)
+void moveqImpl(Dummy)(CpuPtr cpu)
 {
+    const reg = ((cpu.memory.getValue!ubyte(cpu.state.PC - 0x2) >> 1) & 0b111);
     const int data = cpu.memory.getValue!byte(cpu.state.PC - 0x1);
-    cpu.state.D[Reg] = data;
+    cpu.state.D[reg] = data;
     updateFlags(cpu, data);
 }
