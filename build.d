fe04a95bf02eeae23cf323bd6758c7237c4532a0
Ivan Butygin <ivan.butygin@gmail.com>
2015-04-16 22:34:53 +0300

    build script

--- build.d
+++ build.d
@@ -9,6 +9,7 @@ import std.exception;
 import std.conv;
 import std.string;
 import std.json;
+import std.datetime;
 
 void main(string[] args)
 {
@@ -16,6 +17,7 @@ void main(string[] args)
     scope(success) writeln("Success");
     scope(failure) writeln("Failure");
     const string exeName = "smd-emul";
+    const string outputPath = "bin";
     const string currPath = "./";
     const string cacheFile = ".cache";
     const string[] importPaths = ["d-gamelib","source","d-gamelib/gamelib/3rdparty/DerelictUtil-master/source","d-gamelib/gamelib/3rdparty/DerelictSDL2-master/source"];
@@ -45,7 +47,7 @@ void main(string[] args)
     }
     JSONValue cache;
     const cachePath = buildDir ~ cacheFile;
-    if(exists(cacheFile))
+    if(exists(cachePath))
     {
         cache = parseJSON(std.file.readText(cachePath));
     }
@@ -57,11 +59,24 @@ void main(string[] args)
     const buildStr = compiler ~ " " ~ currentOpts ~ " " ~ importPaths.map!(a => format(importOpt,currPath~a)).join(" ") ~ " ";
     auto objFiles = appender!(string[])();
     const outputOpt = outputOpts[compiler];
+
+    if(cache.isNull())
+    {
+        string[string] dummy;
+        cache = JSONValue(["files" : dummy]);
+    }
+    JSONValue cacheFiles = cache.object["files"];
+    if(cacheFiles.isNull())
+    {
+        string[string] dummy;
+        cacheFiles = JSONValue(["foo":"bar"]);
+    }
     writeln("Compiling...");
     //foreach(d; parallel(sourceList, 1))
     foreach(d; sourceList)
     {
-        const file = d;//d.find(currPath)[currPath.length..$].array;
+        const file = d;
+        const prettyFile = d.find(currPath)[currPath.length..$].text;
         enforce(exists(file), format("File not found: %s",file));
         const fileDir = buildDir ~ file.retro.find!(a => a == '\\' || a == '/').retro.text;
         if(!exists(fileDir))
@@ -69,6 +84,15 @@ void main(string[] args)
             mkdirRecurse(fileDir);
         }
         const objFile = fileDir ~ file.retro.find('.').retro.text ~ "obj";
+        scope(success) objFiles ~= objFile;
+        if(exists(objFile) &&
+            (prettyFile in cacheFiles.object) &&
+            timeLastModified(file) == SysTime.fromISOString(cacheFiles[prettyFile].str))
+        {
+            writeln(format("\"%s\" is up to date",prettyFile));
+            continue;
+        }
+        scope(success) cacheFiles.object[prettyFile] = JSONValue(timeLastModified(file).toISOString());
         //writeln(file);
         //writeln(objFile);
         //writeln(fileDir);
@@ -77,10 +101,15 @@ void main(string[] args)
         writeln(cmd);
         const status = executeShell(cmd);
         enforce(0 == status.status, format("Build error %s, output:\n%s", status.status, status.output));
-        objFiles ~= objFile;
     }
-    
-    const cmd = "dmd " ~ objFiles.data.join(" ") ~ " " ~ format(outputOpt,exeName);
+    cache.object["files"] = cacheFiles;
+    const outputDir = currPath ~ outputPath ~ "/"~config~"/";
+    if(!exists(outputDir))
+    {
+        mkdirRecurse(outputDir);
+    }
+
+    const cmd = "dmd " ~ objFiles.data.join(" ") ~ " " ~ format(outputOpt,outputDir~exeName);
     writeln("Linking...");
     writeln(cmd);
     const status = executeShell(cmd);
